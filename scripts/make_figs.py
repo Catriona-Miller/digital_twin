85d65f984541684bc24725458c67d63c4057c988
Catriona-Miller <cmil573@LG432535.UoA.auckland.ac.nz>
2025-11-10 11:17:59 +1300

    code for aa plots update

--- scripts/make_figs.py
+++ scripts/make_figs.py
@@ -247,9 +247,9 @@ def create_individual_dumbbell_plot(subject_id: str,
     
     fig.savefig(f'../Outcomes/dumbbell_plot_{subject_id}_1.png', dpi=300, bbox_inches='tight')
 
-def plot_aa_violins_by_cluster(aa_file_path: str = '../data/aa.tsv', outcomes_dir: str = '../Outcomes'):
+def plot_aa_violins_ratio(aa_file_path: str = '../data/aa.tsv', outcomes_dir: str = '../Outcomes'):
     """
-    Plots violin plots of some AA levels (Tyrosine, Methionine, Aspartic acid, Taurine)
+    Plots violin ratios of essential to non-essential amino acids
     for individuals in each cluster (from pop cluster fig), separately for timepoints 0 and 52.
     """
     plt.style.use('seaborn-v0_8-whitegrid')
@@ -264,7 +264,7 @@ def plot_aa_violins_by_cluster(aa_file_path: str = '../data/aa.tsv', outcomes_di
 
     # get essential and non-essential aas
     essential_aas = ['arginine', 'histidine', 'isoleucine', 'leucine', 'lysine', 'methionine', 'phenylalanine', 'taurine', 'threonine', 'trytophan', 'valine']
-    non_essential_aas = ['alanine', 'asparagine', 'aspartic_acid', 'citrulline', 'glutamic_acid', 'glutamine', 'glycine', 'hydroxyproline', 'proline', 'serine', 'tyrosine']
+    non_essential_aas = ['alanine', 'asparagine', 'aspartic_acid', 'citrulline', 'glutamic_acid', 'glutamine', 'glycine', 'hydroxyproline', 'proline', 'serine', 'tyrosine','ornithine']
 
     aa_0 = aa[aa.index.str.endswith('_0')].copy()
     aa_52 = aa[aa.index.str.endswith('_52')].copy()
@@ -297,7 +297,7 @@ def plot_aa_violins_by_cluster(aa_file_path: str = '../data/aa.tsv', outcomes_di
         annotator.configure(test='Mann-Whitney', text_format='star', loc='inside', verbose=0)
         annotator.apply_and_annotate()
 
-        plt.savefig(os.path.join(outcomes_dir, f'aa_ess_noness_ratio_timepoint_{timepoint}.pdf'), format='pdf', dpi=300, bbox_inches='tight')
+        #plt.savefig(os.path.join(outcomes_dir, f'aa_ess_noness_ratio_timepoint_{timepoint}.pdf'), format='pdf', dpi=300, bbox_inches='tight')
 
     # Plot for timepoint 0
     plot_violins(aa_0, timepoint=0)
@@ -310,6 +310,20 @@ def plot_aa_violins_by_cluster(aa_file_path: str = '../data/aa.tsv', outcomes_di
     aa_diff['ess_noness_ratio'] = aa_diff[essential_aas].sum(axis=1) / aa_diff[non_essential_aas].sum(axis=1)
     plot_violins(aa_diff, timepoint='52-0 (difference)')
 
+    # Add in healthys to timepoint 0
+    meta = pd.read_csv('../data/meta.tsv', sep='\t')
+    healthy_ids = meta[meta['Condition'] == 'Well-nourished']['subjectID'].tolist()
+    aa_healthy = aa.loc[[f'{sid}_0' for sid in healthy_ids if f'{sid}_0' in aa.index]].copy()
+    aa_healthy['cluster_id'] = 3
+    aa_healthy['ess_noness_ratio'] = aa_healthy[essential_aas].sum(axis=1) / aa_healthy[non_essential_aas].sum(axis=1)
+    aa_0_with_healthy = pd.concat([aa_0, aa_healthy], axis=0)
+    plot_violins(aa_0_with_healthy, timepoint='0 with Healthys', pairs=[(1,2), (1,3), (2,3)])
+
+    # print mean ratios for each cluster inc healthys at timepoint 0
+    mean_ratios = aa_0_with_healthy.groupby('cluster_id')['ess_noness_ratio'].mean()
+    print("Mean Essential to Non-Essential AA Ratios at Timepoint 0 (including Healthys):")
+    print(mean_ratios)
+
     # Additionally, plot ratio for just some non-essential AAs
     non_essential_aas = ['alanine', 'serine', 'ornithine']
 
@@ -320,7 +334,87 @@ def plot_aa_violins_by_cluster(aa_file_path: str = '../data/aa.tsv', outcomes_di
     plot_violins(aa_0, timepoint='0 (non-essential subset)')
     plot_violins(aa_52, timepoint='52 (non-essential subset)')
 
+def plot_aa_violins_by_cluster(aa_file_path: str = '../data/aa.tsv', outcomes_dir: str = '../Outcomes'):
+    """
+    Plots violin plots of some AA levels (Valine, leucine and isoleucine)
+    for individuals in each cluster (from pop cluster fig), separately for timepoints 0 and 52.
+    """
+    plt.style.use('seaborn-v0_8-whitegrid')
 
+    # get cluster assignments
+    assign_path = '../Outcomes/subject_cluster_assignments_small.tsv'
+    cl_df = pd.read_csv(assign_path, sep='\t')
+    cl_df = cl_df.set_index(['subjectID'])
+
+    aa = pd.read_csv(aa_file_path, sep='\t')
+    aa = aa.set_index(aa.columns[0])
+
+    # These are ones we are interested in as diff between clusters
+    desired_aas = ['valine', 'leucine', 'isoleucine', 'ornithine']
+    aa = aa[desired_aas]
+
+    aa_0 = aa[aa.index.str.endswith('_0')].copy()
+    aa_52 = aa[aa.index.str.endswith('_52')].copy()
+
+    # add cluster number as col to aa_0
+    aa_0['subjectID'] = aa_0.index.str.replace('_0', '', regex=False)
+    aa_0 = aa_0.join(cl_df, on='subjectID', how='inner')
+    aa_0 = aa_0.drop(columns=['subjectID'])
+
+    # add cluster number as col to aa_52
+    aa_52['subjectID'] = aa_52.index.str.replace('_52', '', regex=False)
+    aa_52 = aa_52.join(cl_df, on='subjectID', how='inner')
+    aa_52 = aa_52.drop(columns=['subjectID'])
+
+    # Plotting function. Also calcs MWU between all pairs
+    def plot_violins(data: pd.DataFrame, timepoint: int, pairs: list = [(1, 2)]):
+        fig, axes = plt.subplots(1, 4, figsize=(30, 15))
+        axes = axes.flatten()
+        clusters = sorted(cl_df['cluster_id'].unique())
+
+        for i, aa_name in enumerate(aa.columns):
+            ax = axes[i]
+            sns.boxplot(
+                x='cluster_id',
+                y=aa_name,
+                data=data,
+                palette='Set2',
+                ax=ax,
+            )
+
+            annot = Annotator(ax, pairs, data=data, x='cluster_id', y=aa_name)
+            annot.configure(test='Mann-Whitney', text_format='star', loc='inside', verbose=0, comparisons_correction='Benjamini-Hochberg')
+            annot.apply_and_annotate()
+
+            ax.set_title(f'{aa_name.replace("_", " ").title()} Levels by Cluster (Timepoint {timepoint})', fontsize=14)
+            ax.set_xlabel('Cluster', fontsize=12)
+            ax.set_ylabel(f'{aa_name.replace("_", " ").title()} Level', fontsize=12)
+
+        plt.tight_layout()
+        out_path = os.path.join(outcomes_dir, f'aa_box_timepoint_{timepoint}.png')
+        plt.savefig(out_path, dpi=300)
+        plt.close()
+    
+    # Plot for timepoint 0
+    plot_violins(aa_0, timepoint=0)
+    # add in healthys to timepoint 0
+    meta = pd.read_csv('../data/meta.tsv', sep='\t')
+    healthy_ids = meta[meta['Condition'] == 'Well-nourished']['subjectID'].tolist()
+    aa_healthy = aa.loc[[f'{sid}_0' for sid in healthy_ids if f'{sid}_0' in aa.index]].copy()
+    aa_healthy['cluster_id'] = 3
+    aa_0_with_healthy = pd.concat([aa_0, aa_healthy], axis=0)
+    plot_violins(aa_0_with_healthy, timepoint='0 with Healthys', pairs=[(1,2), (1,3), (2,3)])
+    # Plot for timepoint 52
+    plot_violins(aa_52, timepoint=52)
+    # also plot the difference between the two (52 - 0). Keep cluster_id as is
+    aa_0_aligned = aa_0.copy()
+    aa_52_aligned = aa_52.copy()
+    aa_0_aligned.index = aa_0_aligned.index.str.replace('_0', '', regex=False)
+    aa_52_aligned.index = aa_52_aligned.index.str.replace('_52', '', regex=False)
+    # get log2 difference of all columns except cluster_id
+    aa_diff = np.log2( aa_52_aligned.drop(columns=['cluster_id']) / aa_0_aligned.drop(columns=['cluster_id']) )
+    aa_diff['cluster_id'] = aa_0_aligned['cluster_id']
+    plot_violins(aa_diff, timepoint='Difference (52-0)')
 
 if __name__ == "__main__":
     intervention_data = pd.read_csv('../Outcomes/population_wlz_deltas_neg1_small.tsv', sep='\t')
@@ -333,7 +427,7 @@ if __name__ == "__main__":
     #create_full_clustermap_clusters(intervention_data, top_n_features=10, n_clusters=2)
 
     ## Below fn has to be run after cluster_intervention.py, other ones above can be either, either, or :)
-    plot_aa_violins_by_cluster(
+    plot_aa_violins_ratio(
         aa_file_path='../data/aa.tsv',
         outcomes_dir='../Outcomes'
     )
